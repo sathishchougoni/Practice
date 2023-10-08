a7b5d76e71bfda03895812e7b8e4a79207714098
Sathish <Sathish@DESKTOP-SQBPOCH>
2023-10-08 13:13:11 +0530

    testcase1 update .....

--- tesg/src/tesg1/testcase1.java
+++ tesg/src/tesg1/testcase1.java
@@ -1,13 +1,13 @@
 package tesg1;
 
-public class Testcase1 {
+public class testcase1 {
 
 	public static void main(String[] args) 
 	{
 		
 
 		System.out.println("Testcase1");
-		
+		System.out.println("testcase2 update...");
 	}
 
 }
