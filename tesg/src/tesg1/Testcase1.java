43511de191e53ab0487e4f20fbf81f2b8eec9605
Sathish <Sathish@DESKTOP-SQBPOCH>
2023-10-08 13:00:41 +0530

    Testcase1 Adding

--- tesg/src/tesg1/Testcase1.java
+++ tesg/src/tesg1/Testcase1.java
@@ -2,9 +2,12 @@ package tesg1;
 
 public class Testcase1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
+		System.out.println("Testcase1");
+		
 	}
 
 }
